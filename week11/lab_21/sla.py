f6139e1e9e46bdf8e2ae16dc62a2077b8504ca29
Maziar Sojoudian <maziar@sojoudian.net>
2024-07-17 09:03:00 -0400

    fix lab 21 solution

--- week11/lab_21/sla.py
+++ week11/lab_21/sla.py
@@ -5,7 +5,7 @@ azure_sql_gatabase_uptime = 99.995 /100
 
 #App Service % uptime X Azure AD B2C % uptime X Azure Application Gateway % uptime X Azure SQL Database % uptime = Total % Uptime
 
-total_uptime = app_service_uptime * azure_ad_b2c_uptime * azure_sql_gatabase_uptime
+total_uptime = app_service_uptime * azure_ad_b2c_uptime * azure_sql_gatabase_uptime * application_gateway_uptime
 print(total_uptime)
 
 # Calculate the total uptime percentage
